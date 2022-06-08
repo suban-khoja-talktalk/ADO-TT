7a9cfb7559ef48b53916ea89f1578faf295e0f37
suban-khoja-talktalk <103242035+suban-khoja-talktalk@users.noreply.github.com>
2022-06-08 18:14:03 +0530

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
-const production = false;
+const production = true;
 
 function serve() {
 	let server;
